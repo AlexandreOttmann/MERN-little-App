71553ba7e84465635a525a74464b1a50d4a34298
frontcodelover <contact@avantjetaisriche.com>
2023-01-28 17:28:38 +0100

    INIT: mongoDB connected

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,30 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(process.env.MONGO_DB, () => {
+	console.log("Connected to MongoDB");
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.post('/send', (req, res) => {
-	const data = req.body
-	
-	res.status(201).send(req.body)
-})
+  const data = req.body;
+
+  res.status(201).send(req.body);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
