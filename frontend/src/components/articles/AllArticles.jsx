ec7829afb449eb8b1641f5b26512126c8707b74c
frontcodelover <contact@avantjetaisriche.com>
2023-01-29 17:12:21 +0100

    FEAT: display post

--- frontend/src/components/articles/AllArticles.jsx
+++ frontend/src/components/articles/AllArticles.jsx
@@ -1,27 +1,30 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function AllArticles() {
-	const [posts, setPosts] = useState([])
-	
-	useEffect(() => {
-		getAllPost()
-	}, [])
+  const [posts, setPosts] = useState([]);
 
+  useEffect(() => {
+    getAllPost();
+  }, []);
 
-	async function getAllPost() {
-		try {
-			const data = await axios.get('http://localhost:8080/v2/allposts')
-			setPosts(data.data);
-		}
-		catch(error) {
-			console.log(error);
-		}
-	}
-	
+  async function getAllPost() {
+    try {
+      const data = await axios.get('http://localhost:8080/v2/allposts');
+      setPosts(data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <>
       <div>AllArticles</div>
+      {posts.map((post) => (
+        <div key={post._id}>
+          <h2>{post.title}</h2>
+        </div>
+      ))}
     </>
   );
 }
