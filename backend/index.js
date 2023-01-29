ebb8dfd6a72da162d8995304d96a2204d33bfbfd
frontcodelover <contact@avantjetaisriche.com>
2023-01-29 16:29:20 +0100

    DELETE: remove comment

--- backend/index.js
+++ backend/index.js
@@ -26,18 +26,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-// app.put('/v2/post/:id', async (req, res) => {
-//   try {
-//     const post = await Post.findByIdAndUpdate(req.params.id, req.body);
-//     if (!post) {
-//       return res.status(404).json('Not found');
-//     }
-//     return res.status(201).json('Post updated');
-//   } catch (err) {
-//     return res.status(500).json('Server error');
-//   }
-// });
-
 app.post('/v2/post', async (req, res) => {
   const dbPosts = req.body;
   try {
