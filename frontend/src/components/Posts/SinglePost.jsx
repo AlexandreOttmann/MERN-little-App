7d55d515341848286918d2491048a1837ce43f5f
frontcodelover <contact@avantjetaisriche.com>
2023-01-29 18:19:33 +0100

    FEAT: delete action with redirect

--- frontend/src/components/Posts/SinglePost.jsx
+++ frontend/src/components/Posts/SinglePost.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import './styles.css'
 
 function SinglePost({ id }) {
   const [post, setPost] = useState({});
-
+	const navigate = useNavigate();
+	
   useEffect(() => {
     getSinglePost(id);
   }, []);
@@ -16,11 +18,21 @@ function SinglePost({ id }) {
     } catch (error) {
       console.log(error);
     }
-  }
+	}
+	
+	function handleDelete(e) {
+		e.preventDefault()
+		const deletePost = () => {
+			axios.delete(`http://localhost:8080/v2/post/${id}`)
+			navigate("/articles")
+		}
+		deletePost()
+	}
 
   return (
-    <>
+		<>
       <Link to='/articles'>Retour à tous les articles</Link>
+			<button className='btn--color' onClick={handleDelete}>Supprimer</button>
       <h1>{post.title}</h1>
       <p>{post.author}</p>
       <p>{post.body}</p>
