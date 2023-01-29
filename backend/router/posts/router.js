b7536025a19647ec339b9bed81546d5d0ef4afa5
frontcodelover <contact@avantjetaisriche.com>
2023-01-29 18:55:37 +0100

    REFACTO: add semicon

--- backend/router/posts/router.js
+++ backend/router/posts/router.js
@@ -4,8 +4,8 @@ import postController from '../../controllers/postController.js';
 
 export const router = express.Router();
 
-router.get('/v2/allposts', postController.getAllPosts )
-router.post('/v2/post', postController.addPost)
-router.get('/v2/post/:id', postController.getPostById)
-router.put('/v2/post/:id', postController.updatePostById)
-router.delete('/v2/post/:id', postController.deletePostById)
+router.get('/v2/allposts', postController.getAllPosts);
+router.post('/v2/post', postController.addPost);
+router.get('/v2/post/:id', postController.getPostById);
+router.put('/v2/post/:id', postController.updatePostById);
+router.delete('/v2/post/:id', postController.deletePostById);
