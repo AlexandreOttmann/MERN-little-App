adcd62074de6db0138e714fef58fce022ba0869e
frontcodelover <contact@avantjetaisriche.com>
2023-01-29 16:22:35 +0100

    FEAT: get post by id

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -11,7 +11,20 @@ const postController = {
     } catch (err) {
       return res.status(500).json('Server error');
     }
-  },
+	},
+	getPostById: async (req, res) => {
+		const params = req.params.id;
+		try {
+			const post = await Post.findOne({ _id: params });
+			if (!post) {
+				return res.status(404).json('Post not found');
+			}
+			return res.status(201).json(post);
+		} catch (err) {
+			return res.status(500).json('Server error');
+		}
+	}
 };
+
 
 export default postController;
--- backend/router/posts/router.js
+++ backend/router/posts/router.js
@@ -6,6 +6,6 @@ export const router = express.Router();
 
 router.get('/v2/allposts', postController.getAllPosts )
 router.post('v2/post')
-router.get('/v2/post/:id')
+router.get('/v2/post/:id', postController.getPostById)
 router.put('/v2/post/:id')
 router.delete('/v2/post/:id')
