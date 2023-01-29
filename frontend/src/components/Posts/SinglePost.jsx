29e7bd699362f207035217ca1c0918b59485cfd3
frontcodelover <contact@avantjetaisriche.com>
2023-01-29 17:55:47 +0100

    FIX: split logical

--- frontend/src/components/Posts/SinglePost.jsx
+++ frontend/src/components/Posts/SinglePost.jsx
@@ -1,12 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 function SinglePost({ id }) {
-	const [post, setPost] = useState(null)
-	
-	return (
-		<div>SinglePost</div>
-	)
+  const [post, setPost] = useState({});
+
+  useEffect(() => {
+    getSinglePost(id);
+  }, []);
+
+  async function getSinglePost(id) {
+    try {
+      const response = await axios.get(`http://localhost:8080/v2/post/${id}`);
+      setPost(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <>
+      <Link to='/articles'>Retour à tous les articles</Link>
+      <h1>{post.title}</h1>
+      <p>{post.author}</p>
+      <p>{post.body}</p>
+    </>
+  );
 }
 
-export default SinglePost
+export default SinglePost;
--- frontend/src/pages/SingleArticle.jsx
+++ frontend/src/pages/SingleArticle.jsx
@@ -1,31 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import SinglePost from '../components/Posts/SinglePost';
 
 function SingleArticle() {
-  const [post, setPost] = useState([]);
+  // const [post, setPost] = useState([]);
   const { id } = useParams();
 
-  useEffect(() => {
-    getSinglePost(id);
-  }, []);
+  // useEffect(() => {
+  //   getSinglePost(id);
+  // }, []);
 
-  async function getSinglePost(id) {
-    console.log(id);
-    try {
-      const response = await axios.get(`http://localhost:8080/v2/post/${id}`);
-      setPost(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  // async function getSinglePost(id) {
+  //   console.log(id);
+  //   try {
+  //     const response = await axios.get(`http://localhost:8080/v2/post/${id}`);
+  //     setPost(response.data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
 
   return (
     <>
-      <Link to='/articles'>Retour à tous les articles</Link>
+      <SinglePost id={id} />
+      {/* <Link to='/articles'>Retour à tous les articles</Link>
       <h1>{post.title}</h1>
       <p>{post.author}</p>
-      <p>{post.body}</p>
+      <p>{post.body}</p> */}
     </>
   );
 }
