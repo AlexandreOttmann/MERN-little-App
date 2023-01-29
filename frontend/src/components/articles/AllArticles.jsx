3326925fd51ece1ddd95f73b0af10d0ca9dd6166
frontcodelover <contact@avantjetaisriche.com>
2023-01-29 17:22:07 +0100

    INIT: add react router

--- frontend/src/components/articles/AllArticles.jsx
+++ frontend/src/components/articles/AllArticles.jsx
@@ -22,7 +22,7 @@ function AllArticles() {
       <div>AllArticles</div>
       {posts.map((post) => (
         <div key={post._id}>
-					<h2>{post.title}</h2>
+					<h2><a href={post._id}>{post.title}</a></h2>
 					<p>{post.author}</p>
 					<p>{post.body}</p>
         </div>
