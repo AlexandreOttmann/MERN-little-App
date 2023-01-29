0a95fff4c27085b576148d85412663fe4372ccc0
frontcodelover <contact@avantjetaisriche.com>
2023-01-29 16:33:26 +0100

    DELETE / route

--- backend/index.js
+++ backend/index.js
@@ -22,10 +22,6 @@ mongoose.connect(process.env.MONGO_DB, () => {
   console.log('Connected to MongoDB');
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
