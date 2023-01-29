63f03e4b2581bf66436160e59a123e3d9979b80b
frontcodelover <contact@avantjetaisriche.com>
2023-01-29 17:09:09 +0100

    FEAT: get all posts form api

--- frontend/src/components/articles/AllArticles.jsx
+++ frontend/src/components/articles/AllArticles.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function AllArticles() {
-	return (
-		<div>AllArticles</div>
-	)
+	const [posts, setPosts] = useState([])
+	
+	useEffect(() => {
+		getAllPost()
+	}, [])
+
+
+	async function getAllPost() {
+		try {
+			const data = await axios.get('http://localhost:8080/v2/allposts')
+			setPosts(data.data);
+		}
+		catch(error) {
+			console.log(error);
+		}
+	}
+	
+  return (
+    <>
+      <div>AllArticles</div>
+    </>
+  );
 }
 
-export default AllArticles
+export default AllArticles;
